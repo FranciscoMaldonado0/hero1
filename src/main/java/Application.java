29124da7053cdd627f66e0de6ad63f7412a3d3d9
FranciscoMaldonado0 <up202004244@up.pt>
2021-11-05 15:42:43 +0000

    na classe game

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,33 +8,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args){/*
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        Screen screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null); // we don't need a
-        //cursor
-        screen.startScreen(); // screens must be
-        //started
-        screen.doResizeIfNecessary(); // resize screen if
-        //necessary */
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a
-            //cursor
-            screen.startScreen(); // screens must be
-            //started
-            screen.doResizeIfNecessary(); // resize screen
-            //if necessary
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            terminal = terminalFactory.createTerminal();
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args){
+        Game game = new Game();
 
     }
 }
