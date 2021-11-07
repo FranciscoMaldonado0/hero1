f90cbd1f86dad0bb72cbee1c1cbfd1d7717b6cd1
FranciscoMaldonado0 <up202004244@up.pt>
2021-11-07 23:29:03 +0000

    A tecla já mexe

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,32 +11,52 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
-    public Game() {
+    private int x = 10;
+    private int y = 10;
+    public Game() throws IOException {
         try {
+
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a
-            //cursor
-            screen.startScreen(); // screens must be
-            //started
-            screen.doResizeIfNecessary(); // resize screen
-            //if necessary
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+
             TerminalSize terminalSize = new TerminalSize(40, 20);
             DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
             terminal = terminalFactory.createTerminal();
+
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);screen.refresh();
+
         } catch (IOException e) {
             e.printStackTrace();
+
         }
+
     }
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('Y')[0]);
         screen.refresh();
     }
     public void run() throws IOException{
-        draw();
+        while(true){
+            draw();
+        KeyStroke key = screen.readInput();
+        processKey(key);
+        }
+    }
+    private void processKey(KeyStroke key) throws IOException{
+        System.out.println(key);
+        if (key.getKeyType() == KeyType.ArrowUp){
+            y--;
+        }
+        else if (key.getKeyType() == KeyType.ArrowDown)
+            y++;
+        else if (key.getKeyType() == KeyType.ArrowLeft)
+            x--;
+        else if (key.getKeyType() == KeyType.ArrowRight)
+            x++;
     }
 }
